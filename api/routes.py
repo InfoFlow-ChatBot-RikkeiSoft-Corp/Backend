c2d963ca73ee8b5381c22660817c7fa78f421016
ZZKIM <rkgml424@naver.com>
2025-01-08 11:01:55 +0700

    del: delete un-used file

--- api/routes.py
+++ api/routes.py
@@ -12,7 +12,6 @@ api_bp = Blueprint('api', __name__)
 # 클래스 인스턴스 생성
 document_fetcher = DocumentFetcher()
 vector_db_manager = VectorDBManager()
-query_generator = QueryGenerator()
 retriever_manager = RetrieverManager()
 answer_generator = AnswerGenerator(model="models/gemini-1.5-flash", temperature=0)
 rag_service = RAGService(retriever_manager, answer_generator)
@@ -36,22 +35,6 @@ def ask():
 def home():
     docs_list = vector_db_manager.get_submitted_docs()  # 제출된 뉴스 문서 리스트
     return render_template("index.html", docs_list=docs_list)
-
-
-# 질문 입력 및 쿼리 생성 API
-@api_bp.route("/generate-query", methods=["POST"])
-def generate_query():
-    user_question = request.json.get("question")
-    if not user_question:
-        return jsonify({"error": "❌ 질문을 입력해주세요!"}), 400
-
-    try:
-        # 쿼리 생성
-        query_body = query_generator.build_query(user_question)
-        print(query_body)
-        return jsonify({"query": query_body}), 200
-    except Exception as e:
-        return jsonify({"error": f"❌ 오류 발생: {str(e)}"}), 500
 
 # 벡터 DB 구축 엔드포인트
 @api_bp.route("/build-vector-db", methods=["POST"])
