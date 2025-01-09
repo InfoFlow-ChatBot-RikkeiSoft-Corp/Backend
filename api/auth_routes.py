bc872a5edbc5bdc4730d12758f1114a60fd59633
tootsy <tootsydeshmukh@gmail.com>
2025-01-09 17:38:10 +0700

    update module path

--- api/auth_routes.py
+++ api/auth_routes.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 from flask import Blueprint, request, jsonify, redirect, url_for
-from services.models import db, User, Log, Token, FileMetadata
-=======
+from models.models import db, User, Log, Token, FileMetadata
 from flask import Blueprint, request, jsonify
 from models.models import db, User, Log
->>>>>>> 3cce88b7bfaef9686e8ffc8c27fb39a63a285319
 import sqlalchemy as sa
 from datetime import datetime, timedelta
 import jwt
