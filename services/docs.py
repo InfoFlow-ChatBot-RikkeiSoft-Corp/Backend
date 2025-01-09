fb144decf404a723585e8c6dc341b165ba5b7f26
ScientistLim <jhll0815@naver.com>
2025-01-09 17:03:44 +0900

    fix

--- services/docs.py
+++ services/docs.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from langchain.schema import Document as LangChainDocument
 
 class Docs:
     def __init__(self, title, url, content):
@@ -11,7 +12,24 @@ class Docs:
             "url": self.url
         }
 
+    def to_langchain_document(self):
+        """Convert to LangChain-compatible Document object."""
+        return LangChainDocument(
+            page_content=self.content,
+            metadata={"title": self.title, "url": self.url}
+        )
 
     def get_excerpt(self, length=300):
         """본문 내용의 일부를 반환 (기본 300자)"""
         return self.content[:length] + "..." if len(self.content) > length else self.content
+
+    @staticmethod
+    def from_web(title, url, content):
+        """웹 문서 데이터를 기반으로 Docs 객체 생성"""
+        return Docs(title=title, url=url, content=content)
+
+    @staticmethod
+    def from_file(file_path, content):
+        """파일 데이터를 기반으로 Docs 객체 생성"""
+        title = file_path.split("\\")[-1].split(".")[0]
+        return Docs(title=title, url=file_path, content=content)
